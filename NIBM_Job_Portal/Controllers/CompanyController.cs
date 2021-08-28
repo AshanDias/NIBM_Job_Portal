d280057ecd16cece889d31367b0107696f0c33da
Fayas Ahamed <fayas.emedia@gmail.com>
2021-08-28 21:17:25 +0530

    company profile update method added

---
+++ NIBM_Job_Portal/Controllers/CompanyController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using NIBM_Job_Portal.Interface;
+using NIBM_Job_Portal.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NIBM_Job_Portal.Controllers
+{
+    public class CompanyController : Controller
+    {
+        private readonly ICompanyService _comapnyService;
+        private readonly IWebHostEnvironment webHostEnvironment;
+        public CompanyController(ICompanyService comapnyService, IWebHostEnvironment hostEnvironment)
+        {
+            _comapnyService = comapnyService;
+            webHostEnvironment = hostEnvironment;
+        }
+
+        [HttpPost]
+        public IActionResult UpdateCompany(Company company)
+        {
+            _comapnyService.UpdateCompany(company);
+            return View();
+        }
+
+    }
+}
---
+++ NIBM_Job_Portal/Interface/ICompanyService.cs
@@ -0,0 +1,13 @@
+﻿using NIBM_Job_Portal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NIBM_Job_Portal.Interface
+{
+    public interface ICompanyService
+    {
+        void UpdateCompany(Company company);
+    }
+}
