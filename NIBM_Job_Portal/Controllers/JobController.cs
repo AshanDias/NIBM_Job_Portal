7e06ad1376ec065bb5b4313efc1c490b46a33ecf
Fayas Ahamed <fayas.emedia@gmail.com>
2021-09-01 22:38:52 +0530

    job controller methods

--- NIBM_Job_Portal/Controllers/JobController.cs
+++ NIBM_Job_Portal/Controllers/JobController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NIBM_Job_Portal.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,62 @@ namespace NIBM_Job_Portal.Controllers
 {
     public class JobController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _applicationDbContext;
+        public JobController(ApplicationDbContext applicationDbContext)
         {
+
+            _applicationDbContext = applicationDbContext;
+        }
+        public ActionResult Index()
+        {
+            var jobs = _applicationDbContext.Job.ToList();
+            return View(jobs);
+
+        }
+
+        [HttpPost]
+        public ActionResult Create(Job model)
+        {
+            _applicationDbContext.Job.Add(model);
+            _applicationDbContext.SaveChanges();
+            ViewBag.Message = "Data Insert Successfully";
             return View();
         }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var data = _applicationDbContext.Job.Where(x => x.Id == id).FirstOrDefault();
+            return View(data);
+        }
+        [HttpPost]
+        public ActionResult Edit(Job model)
+        {
+            var data = _applicationDbContext.Job.Where(x => x.Id == model.Id).FirstOrDefault();
+            if (data != null)
+            {
+                data.Position = model.Position;
+                data.JobCategoryId = model.JobCategoryId;
+                data.Description = model.Description;
+                data.CompanyId = model.CompanyId;
+                _applicationDbContext.SaveChanges();
+            }
+
+            return RedirectToAction("index");
+        }
+        public ActionResult Delete(int id)
+        {
+            var data = _applicationDbContext.Job.Where(x => x.Id == id).FirstOrDefault();
+            _applicationDbContext.Job.Remove(data);
+            _applicationDbContext.SaveChanges();
+            ViewBag.Messsage = "Record Delete Successfully";
+            return RedirectToAction("index");
+        }
+        public ActionResult View(int id)
+        {
+            var data = _applicationDbContext.Job.Where(x => x.Id == id).FirstOrDefault();
+            return View(data);
+        }
+
     }
 }
