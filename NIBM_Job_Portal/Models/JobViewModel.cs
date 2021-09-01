58b57a68d56a94050603daf5dc20605206a2c905
NIPUNSGITHUB <nipunishara.info@gmail.com>
2021-09-02 00:20:17 +0530

    Change name in JOB LIST and Add files to index and create respectively

--- NIBM_Job_Portal/Models/JobViewModel.cs
+++ NIBM_Job_Portal/Models/JobViewModel.cs
@@ -6,11 +6,14 @@ using System.Threading.Tasks;
 namespace NIBM_Job_Portal.Models
 {
     public class JobViewModel
-    {
+    { 
         public int Id { get; set; }
         public string Position { get; set; }
         public int JobCategoryId { get; set; }
         public string Description { get; set; }
+        public string jobFlyer { get; set; }
         public int CompanyId { get; set; }
+        public JobCategory JobCategory { get; set; }
+        public Company Company { get; set; }
     }
 }
