e726add43647b6c19d8459fcfe76b6ca4e0004cd
NIPUNSGITHUB <nipunishara.info@gmail.com>
2021-09-09 20:23:24 +0530

    Add admin sign In UI

---
+++ NIBM_Job_Portal/Controllers/AdminAccountController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NIBM_Job_Portal.Controllers
+{
+    public class AdminAccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
