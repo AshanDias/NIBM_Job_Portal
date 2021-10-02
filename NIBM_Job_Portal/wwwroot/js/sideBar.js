5a77fc0d227686dd98879090d4734bc04a3f8925
NIPUNSGITHUB <nipunishara.info@gmail.com>
2021-10-02 11:27:20 +0530

    Add job flyer image active sidebar

--- NIBM_Job_Portal/wwwroot/js/sideBar.js
+++ NIBM_Job_Portal/wwwroot/js/sideBar.js
@@ -2,16 +2,16 @@
     resetActiveClass();
     switch (buttonID) {
         case "sidebar_dashboard":
-            $("#sidebar_dashboard").addClass("active");
+            $("#sidebar_dashboard").addClass("text-primary");
             break;
         case "sidebar_job":
-            $("#sidebar_job").addClass("active");
+            $("#sidebar_job").addClass("text-primary");
             break;
         case "sidebar_profile":
-            $("#sidebar_profile").addClass("active");
+            $("#sidebar_profile").addClass("text-primary");
             break;
         case "sidebar_company":
-            $("#sidebar_company").addClass("active");
+            $("#sidebar_company").addClass("text-primary");
             break; 
     } 
 }
