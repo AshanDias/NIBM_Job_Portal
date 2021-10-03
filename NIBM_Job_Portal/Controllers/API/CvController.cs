10396fa980c3128266c2d88c26b0ae8583fa94d1
ashan.dias <ashan.dias>
2021-10-03 20:13:32 +0530

    GetAllCv

--- NIBM_Job_Portal/Controllers/API/CvController.cs
+++ NIBM_Job_Portal/Controllers/API/CvController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -37,6 +38,27 @@ namespace NIBM_Job_Portal.Models.API
 
 
                 return Ok(request);
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+
+        }
+
+        [HttpGet]
+        [Route("getallcv")]
+        public async Task<IActionResult> GetAllCv(int id)
+        {
+            try
+            {
+                List<string> vcs = new List<string>();
+                var students = await _applicationDbContext.CVDocs.Where(x => x.StudentId == id).ToListAsync();
+                foreach (var item in students)
+                {
+                    vcs.Add(item.document_url);
+                }
+                return Ok(vcs);
             }
             catch
             {
